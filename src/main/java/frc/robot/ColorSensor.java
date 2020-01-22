585c4902e0073ed697aef46dfb8fd6b3340319a1
Julia Holz <julia@Julias-MacBook-Air-2.local>
2020-01-21 20:50:40 -0500

    Added actual code

--- src/main/java/frc/robot/ColorSensor.java
+++ src/main/java/frc/robot/ColorSensor.java
@@ -7,8 +7,42 @@
 
 package frc.robot;
 
-/**
- * Add your docs here.
- */
-public class ColorSensor {
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorSensorV3.RawColor;
+import edu.wpi.first.wpilibj.I2C;
+
+public class ColorSensorV{
+    private final ColorSensorV3 colorSensor;
+    enum Color
+    {
+        RED, GREEN, BLUE, YELLOW, NONE
+    }
+    public ColorSensorV(){
+        colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
+    }
+
+    public RawColor getRawColor(){
+        //return a Raw Color object, which has int attributes for red, green, blue, and infrared
+        return colorSensor.getRawColor();
+    }
+
+    public Color getColor()
+    {
+        RawColor color = getRawColor();
+    
+        //These constants decide what is considered "full enough" or "empty enough" for a given value, since RGB values are 0 or 250
+        final int FULL = 250;
+        final int EMPTY = 5;
+
+        if(color.red<= EMPTY && color.green>=FULL && color.blue>=FULL) return Color.BLUE;
+        else if(color.red>=FULL && color.green>=FULL && color.blue<=EMPTY) return Color.YELLOW;
+        else if(color.red <= EMPTY && color.green>=FULL && color.blue<=EMPTY ) return Color.GREEN;
+        else if(color.red>=FULL && color.green<=EMPTY && color.blue<=EMPTY ) return Color.RED;
+        else return Color.NONE;
+
+
+    }
+    
+
+
 }
